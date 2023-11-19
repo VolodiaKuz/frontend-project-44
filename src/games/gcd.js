ad17ccd18dc3c49cee11acbaddcbb37dcd007e00
Volodia K <wtraktorman94@mail.ru>
2023-11-19 23:52:13 +0600

    fix brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,46 +1,39 @@
 import readlineSync from 'readline-sync';
-import { getRandomInt, greeting, check } from '../index.js';
+import { getRandomInt, startGame } from '../index.js';
 
-const gcd = () => {
-  const name = greeting();
+const rules = 'Find the greatest common divisor of given numbers.';
 
-  for (let i = 0; i < 3; i += 1) {
-    const a = getRandomInt(1, 50) * 2;
-    const b = getRandomInt(1, 50) * 2;
-    // сделать массив с подходящими числами и брать из него случайный эллемент
+const brainGcd = () => {
+  const a = getRandomInt(1, 50) * 2;
+  const b = getRandomInt(1, 50) * 2;
+  // сделать массив с подходящими числами и брать из него случайный эллемент 
 
-    console.log('Find the greatest common divisor of given numbers.');
-    console.log(`Question: ${a} ${b}`);
-
-    let biggestNumber;
-    let smallestNumber;
-    if (a > b) {
-      biggestNumber = a;
-      smallestNumber = b;
-    } else {
-      biggestNumber = b;
-      smallestNumber = a;
-    }
-
-    let remainder;
-    let nod;
-    while (remainder !== 0) {
-      remainder = biggestNumber % smallestNumber;
-      if (remainder === 0) {
-        nod = smallestNumber;
-        break;
-      }
-      biggestNumber = smallestNumber;
-      smallestNumber = remainder;
-    }
-
-    // console.log(`НОД = ${nod}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    const r = check(userAnswer, String(nod), name);
-    if (r === false) return;
+  let biggestNumber;
+  let smallestNumber;
+  if (a > b) {
+    biggestNumber = a;
+    smallestNumber = b;
+  } else {
+    biggestNumber = b;
+    smallestNumber = a;
   }
-  console.log(`Congratulations, ${name}!`);
+
+  let remainder;
+  let nod;
+  while (remainder !== 0) {
+    remainder = biggestNumber % smallestNumber;
+    if (remainder === 0) {
+      nod = smallestNumber;
+      break;
+      }
+    biggestNumber = smallestNumber;
+    smallestNumber = remainder;
+    }
+
+  const correctAnswer = nod;
+  const question = `Question: ${a} ${b}`;
+  return [question, correctAnswer, rules];
 };
 
-export default gcd;
+const startBrainGcd = () => startGame(brainGcd);
+export default startBrainGcd;
