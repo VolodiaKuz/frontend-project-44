8073270dbc494ec8c7b76a59438bad489ea91d1c
Volodia K <wtraktorman94@mail.ru>
2023-11-20 00:10:16 +0600

    fix brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,31 +1,25 @@
-import readlineSync from 'readline-sync';
-import { getRandomInt, greeting, check } from '../index.js';
+import { getRandomInt, startGame } from '../index.js';
 
-const progression = () => {
-  const name = greeting();
+const rules = 'What number is missing in the progression?';
 
-  for (let i = 0; i < 3; i += 1) {
-    const firstElement = getRandomInt(0, 20);
-    const arr1 = [firstElement];
-    const elementIncrement = getRandomInt(0, 6) + 1;
+const brainProgression = () => {
+  const firstElement = getRandomInt(0, 20);
+  const arr1 = [firstElement];
+  const elementIncrement = getRandomInt(0, 6) + 1;
 
-    for (let j = 0; j < 9; j += 1) {
-      arr1.push(arr1[j] + elementIncrement);
-    }
-
-    const randomElement = getRandomInt(0, 10);
-    const correctAnswer = arr1[randomElement];
-    arr1[randomElement] = '..';
-
-    const arrayWithSpace = arr1.join(' ');
-    console.log('What number is missing in the progression?');
-    console.log(`Question: ${arrayWithSpace}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    const r = check(userAnswer, String(correctAnswer), name);
-    if (r === false) return;
+  for (let j = 0; j < 9; j += 1) {
+    arr1.push(arr1[j] + elementIncrement);
   }
-  console.log(`Congratulations, ${name}!`);
+
+  const randomElement = getRandomInt(0, 10);
+  const correctAnswer = arr1[randomElement];
+  arr1[randomElement] = '..';
+
+  const arrayWithSpace = arr1.join(' ');
+
+  const question = `Question: ${arrayWithSpace}`;
+  return [question, correctAnswer, rules];
 };
 
-export default progression;
+const startBrainProgression = () => startGame(brainProgression);
+export default startBrainProgression;
